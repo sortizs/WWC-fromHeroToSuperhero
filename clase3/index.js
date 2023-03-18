aafae27a3160e9ef5e90c1e0ff63be9153530f8d
Sebastian <sortizs@outlook.com>
2023-03-18 11:48:52 -0500

    Clase3: RESTful API GET & POST

--- clase3/index.js
+++ clase3/index.js
@@ -4,16 +4,59 @@ const HOST = "localhost";
 const PORT = 8000;
 
 function writeHtmlResponse(res, htmlCode) {
-    res.setHeader("Content-Type", "text/HTML");
-    res.writeHead(200);
-    res.end(htmlCode);
-};
+  res.setHeader("Content-Type", "text/HTML");
+  res.writeHead(200);
+  res.end(htmlCode);
+}
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    writeHtmlResponse(res, `<h1>Codigo HTML en ${url}</h1>`);
+function getProducts(res, json) {
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(JSON.stringify(json));
+}
+
+const products = [
+  {
+    name: "Reloj",
+    price: 300,
+    quantity: 2,
+  },
+  {
+    name: "Correa",
+    price: 100,
+    quantity: 6,
+  },
+  {
+    name: "Sombrero",
+    price: 50,
+    quantity: 3,
+  },
+];
+
+const server = http.createServer(async (req, res) => {
+  let body = "";
+  const url = req.url;
+  const method = req.method;
+
+  // Descargar y juntar los datos de la petición
+  await req.on("data", (chunk) => (body += chunk));
+
+  switch (url) {
+    case "/api/v1/products":
+      if (method === "GET") {
+        getProducts(res, products);
+      } else if (method === "POST") {
+        const product = JSON.parse(body)
+        products.push(product);
+        getProducts(res, products);
+      }
+      break;
+    default:
+      writeHtmlResponse(res, `<h1>Codigo HTML en ${url}</h1>`);
+      break;
+  }
 });
 
 server.listen(PORT, HOST, () => {
-    console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
-})
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+});
